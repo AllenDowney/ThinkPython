fdb67a2d4ce76ae05e10bc707179d19f2dbd25fc
Oppong-Agyare ANOKYE NKANSAH <72044731+oanokyen@users.noreply.github.com>
2020-11-04 23:35:06 +0000

    Exercise 7.1 in ThinkPython2 book
    Uploaded my solution to exercise 7.1 in ThinkPython2 book. 
    I found the solutions online quite complicated for someone new to Python.

---
+++ ex_7_1.py
@@ -0,0 +1,26 @@
+import math
+
+def cal(a):  # square root using numerical method type iteration
+    x = a/4  # estimated value to start iteration
+    while True:
+        # print(x)  #Muted to only show final value
+        y=(x+(a/x))/2
+        if y == x:
+            return y 
+        x=y    
+
+
+def test(a):  
+    e= cal(a)
+    f=(math.sqrt(a))
+    diff =(f-e)
+    return print('Number:',a,'MySqrt:',e,'PySqrt:',f,'Difference:',diff)  #NB 'a' is value to be found
+
+        
+        
+n=1.0
+while n<10.0:
+    a=n
+    test(a)
+    n=n+1.0
+    
